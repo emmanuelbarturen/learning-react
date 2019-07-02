a5c16e13137cc4282ecf8860d3b9f310706207a3
Emmanuel <emmanuelbarturen@gmail.com>
2019-07-02 11:01:36 -0500

    install native-base

--- App.js
+++ App.js
@@ -7,8 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-/*import {DrawerNavigator} from 'react-navigation'*/
+import {StyleSheet, Text, View} from 'react-native';
+import {DrawerNavigator} from 'react-navigation'
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -17,19 +17,20 @@ export default class App extends Component<Props> {
             <View style={styles.container}>
                 <Text style={styles.welcome}>Menú</Text>
                 <Text style={styles.instructions}>Estilo hamburguesa.</Text>
+                <Text>mola</Text>
             </View>
         );
     }
 }
 
-/*const myApp = DrawerNavigator({
+const myApp = DrawerNavigator({
     Home: {
         screen: HomeScreen
     },
     Settings: {
         screen: SettingScreen
     }
-});*/
+});
 
 const styles = StyleSheet.create({
     container: {
@@ -41,7 +42,8 @@ const styles = StyleSheet.create({
     welcome: {
         fontSize: 20,
         textAlign: 'center',
-        margin: 10
+        margin: 10,
+        textDecorationLine: "underline",
     },
     instructions: {
         textAlign: 'center',
